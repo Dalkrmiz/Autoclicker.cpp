3ca2dc58ca6b794c8d05449b8ea54c1c2b8b445b
Dalkrmiz <156049172+Dalkrmiz@users.noreply.github.com>
2024-02-20 22:26:41 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,30 +6,28 @@ using namespace std;
 
 void clicker() {
 
-	bool isClicking = false;
-		while (true)
-		{
-			if (GetAsyncKeyState('X')) // checks if key X is pressed
-			{
-				isClicking = true;
-			}
+bool isClicking = false;
+while (true)
+{
+	if (GetAsyncKeyState('X')) // checks if key X is pressed
+	{
+		isClicking = true;
+	}
 
-			else if (GetAsyncKeyState('Z')) 
-			{
-				isClicking = false;
-			}
+	else if (GetAsyncKeyState('Z')) 
+	{
+		isClicking = false;
+	}
 
-			if (isClicking == true)
-			{
-				mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-				mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-				Sleep(5); // delay between clicks
-			}
+	if (isClicking == true)
+	{
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+		Sleep(5); // delay between clicks
+	}
 		
-		};
-
+};
 }
-
 
 int main()
 {
